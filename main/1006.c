bd25c70df87883ad5cff54cb04f394d627fb2005
ctx080415 <65986138+ctx080415@users.noreply.github.com>
2022-11-12 00:31:19 +0900

    Add files via upload

---
+++ main/1006.c
@@ -0,0 +1,16 @@
+/* 
+다음 문장을 출력하시오.
+
+"!@#$%^&*()"
+(단, 큰따옴표도 함께 출력한다.)
+링크: https://codeup.kr/problem.php?id=1006
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  printf("\"!@#$%^&*()\"");
+  return 0;
+}
