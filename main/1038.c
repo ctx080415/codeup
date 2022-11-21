215908407d2f8fa56d178fc134f6cd51d2c63326
ctx080415 <65986138+ctx080415@users.noreply.github.com>
2022-11-21 23:08:40 +0900

    1038

---
+++ main/1038.c
@@ -0,0 +1,16 @@
+/* 정수 2개를 입력받아 합을 출력하는 프로그램을 작성해보자.
+(단, 입력되는 정수는 -1073741824 ~ 1073741824 이다.)
+링크: https://codeup.kr/problem.php?id=1038
+ */
+
+#include <stdio.h>
+
+int main() {
+    long int x; // 변수 선언
+    long int y; // long int로 선언한 이유는, int의 범위가 –2,147,483,648 ~ 2,147,483,647인데, 만약 입력값이 1073741824가 두 번 나온다면, 값을 넘어서기에 long int로 선언한다.
+
+    scanf("%ld %ld", &x, &y); // x와 y로 값 받기
+    printf("%ld", x + y);
+
+    return 0;
+}
