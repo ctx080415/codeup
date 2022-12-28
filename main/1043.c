a558076c1f47bf75ec1d0a5b764e19be54e4d047
ctx080415 <65986138+ctx080415@users.noreply.github.com>
2022-12-28 23:54:58 +0900

    Update 1043.c

--- main/1043.c
+++ main/1043.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(void) {
-    int x;
+    int x; // 변수 선언
     int y;
     
-    scanf("%d %d", &x, &y);
-    printf("%d", x % y);
+    scanf("%d %d", &x, &y); // 공백을 사이로 문자 입력받기
+    printf("%d", x % y); // % 연산자를 이용하여 나머지 출력
     
     return 0;
 }
